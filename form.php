eaaaede213bb4e3f76962c032314f8a5f6cc453d
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2021-01-28 18:20:38 -0500

    1010

--- form.php
+++ form.php
@@ -17,5 +17,16 @@
   <input type="submit" name="submit"><br>
   <input type="reset" name="reset">
   </form>
+  
+  First Name: <input type="text" name="FirstName" value = ""><br>
+  Last Name: <input type="text" name="LastName" value = ""><br>
+  Age: <input type="number" name="Age" value = ""><br>
+  Gender: <input type="radio" name="Gender" value = "">Male
+  <input type="radio" name="Gender" value = "">Female
+  <input type="radio" name="Gender" value = "">Not willing <br>
+  E-mail: <input type="text" name="email" value = ""><br>
+  <input type="submit" name="submit"><br>
+  <input type="reset" name="reset">
+  </form>
 </body>
 </html>
